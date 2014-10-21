3cc277e7d2a55f9684481172f763565cf25c07cc
Nicholas van de Walle <nicholas@niceguys.co>
2014-10-21 00:20:54 -0700

    Did some hacky changes which I will need to undo sometime soon

--- app/index.js
+++ app/index.js
@@ -5,7 +5,16 @@ const TWILIO_ID = process.env.TWILIO_ID;
 const TWILIO_AUTH = process.env.TWILIO_AUTH; 
 
 var app = express();
-var texter = require('./texter')(TWILIO_ID, TWILIO_AUTH);
+
+if (process.env.TWILIO_ID) {
+  var texter = require('./texter')(TWILIO_ID, TWILIO_AUTH);
+  // Want to just get this out there for tonight so ping knows what's what
+  texter.sendLove(process.env.PING_PHONE, function(err) {
+    if (err) throw err;
+
+    console.log('Love sent');
+  });
+}
 
 app.set('port', (process.env.PORT || 5000))
 
