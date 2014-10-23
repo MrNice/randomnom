cdd32b5755c8275a2a5a00ae6fadb8bde1d98811
Nicholas van de Walle <nicholas@niceguys.co>
2014-10-23 12:14:33 -0700

    PORT constant now defaults to 5000

--- app/constants.js
+++ app/constants.js
@@ -1,6 +1,6 @@
 module.exports = {
   // Environmental Variables
-  PORT: process.env.PORT || null,
+  PORT: process.env.PORT || 5000,
   TWILIO_ID: process.env.TWILIO_ID || null,
   TWILIO_AUTH: process.env.TWILIO_AUTH || null,
   MY_PHONE: process.env.MY_PHONE || null
